c05d5396ab93a9b50e52eeb6a38d6461826d1bfb
SukkaW <isukkaw@gmail.com>
2026-03-01 17:15:02 +0800

    Feat: build `index.d.ts`

---
+++ dts-src.ts
@@ -0,0 +1,12 @@
+// This file is used to generate the type definitions for the package, which
+// re-exports all the types from the "color" package, and also exports the
+// default export from the "color" package.
+//
+// `dts-bundle-generator` is used to generate the type definitions from this
+// file.
+//
+// You can run `npm run build:dts` to generate the type definitions, the
+// generated type definitions will be output to `index.d.ts`.
+
+export * from "color";
+export { default } from "color";
